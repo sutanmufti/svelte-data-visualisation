5366afb8e8dbee1360be5f79f57feef0e565df9d
sutan mufti <upsutan@gmail.com>
2023-03-19 18:13:43 +0000

    binding

---
+++ src/routes/binding/+page.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    import { onMount } from "svelte";
+    let buttonNames = [0,1,2,3]
+    let buttons: HTMLButtonElement[] = []
+
+    let initiated = false;
+    onMount(()=>{
+        if (!initiated){
+            buttons.forEach(_=>{
+                _.click()
+            })
+        }
+        initiated = true
+    })
+</script>
+{#each buttonNames as d,i}
+<div>
+    <button on:click={()=>{
+        alert("hello from button! this is self clicked in onMount call back. binding is awesome! button number: " + i);
+        buttons[i].innerHTML = "Button " + i + " was clicked"
+        }} bind:this={buttons[i]}>button {i}</button>
+</div>
+{/each}
