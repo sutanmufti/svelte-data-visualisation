620b356cb6c3965c8e359094e0605b963148500f
sutan mufti <upsutan@gmail.com>
2023-03-19 17:44:24 +0000

    randomiser

---
+++ src/routes/randomiser/+page.svelte
@@ -0,0 +1,7 @@
+<!-- ./randomiser.svelte -->
+<script lang='ts'>
+    let theValue: number = 40;
+    function randomiser(){theValue = Math.floor(Math.random() * 100);}
+  </script>
+  <button on:click={randomiser}>random!</button><br>
+  <p>the value: {theValue}</p>
